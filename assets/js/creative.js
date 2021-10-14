c395a0bee143132be78c388d690b92ab525aeba1
Kevin Kennedy <65367771+iKennedy55@users.noreply.github.com>
2021-10-13 18:24:46 -0600

    Add files via upload

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -52,7 +52,7 @@ window.onscroll = function () {
   
   // show or hide the back-top-top button
   var backToTo = document.querySelector(".scroll-top");
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 500) {
       backToTo.style.display = "flex";
   } else {
       backToTo.style.display = "none";
