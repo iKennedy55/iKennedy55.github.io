00e9cbef4aae2c99b8ecb3f510d7c532f719543a
Kevin Kennedy <65367771+iKennedy55@users.noreply.github.com>
2021-10-13 18:14:26 -0600

    Add files via upload

--- assets/js/creative.js
+++ assets/js/creative.js
@@ -46,3 +46,15 @@
   }
 
 })(); // End of use strict
+
+
+window.onscroll = function () {
+  
+  // show or hide the back-top-top button
+  var backToTo = document.querySelector(".scroll-top");
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+      backToTo.style.display = "flex";
+  } else {
+      backToTo.style.display = "none";
+  }
+};
